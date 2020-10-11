c8932874f3ce589b5c2f0543124eeff5ac6db155
Joseph Wyman <jwymandev@gmail.com>
2020-10-11 11:00:51 -0400

    Embed youtube bastos video

--- screens/centers/videoCenter/VideoPlayerScreen.js
+++ screens/centers/videoCenter/VideoPlayerScreen.js
@@ -1,6 +1,7 @@
 //import liraries
 import React, { Component, useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import WebView from 'react-native-webview';
 
 // create a component
 const VideoPlayerScreen = ({route:{params:{videoData}}}) => {
@@ -9,6 +10,12 @@ const VideoPlayerScreen = ({route:{params:{videoData}}}) => {
     return (
         <View style={styles.container}>
             <Text>{videoData.title}</Text>
+
+            <View style={styles.videoContainer}>
+            <WebView source={{uri: `https://www.youtube.com/embed/${videoData.videoUrl}`}} />
+
+            </View>
+
         </View>
     );
 };
@@ -21,6 +28,11 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         backgroundColor: '#2c3e50',
     },
+    videoContainer:{
+        width:500,
+        height:300,
+        backgroundColor:'red'
+    }
 });
 
 //make this component available to the app
