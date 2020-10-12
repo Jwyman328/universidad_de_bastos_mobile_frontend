7bf4ad16ad01c38f3765936a0526515f9b37d874
Joseph Wyman <jwymandev@gmail.com>
2020-10-12 17:52:46 -0400

    Add video player header

--- getEnvVars/getBackendBaseRoute.js
+++ getEnvVars/getBackendBaseRoute.js
@@ -1,7 +1,6 @@
 import React from 'react'
 
 export  const getBackendBaseRoute = (process) => {
-    console.log('process', process)
     if(process === 'production'){
         return 'https://universidad-de-bastos.herokuapp.com'
     }else{
--- screens/centers/videoCenter/VideoPlayerScreen.js
+++ screens/centers/videoCenter/VideoPlayerScreen.js
@@ -1,38 +1,42 @@
 //import liraries
-import React, { Component, useEffect } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, {Component, useEffect} from 'react';
+import {View, Text, StyleSheet} from 'react-native';
 import WebView from 'react-native-webview';
+import VideoPlayerHeader from '../../../components/headers/VideoPlayerHeader';
+import { primaryGradient } from '../../../styles/colors';
 
 // create a component
-const VideoPlayerScreen = ({route:{params:{videoData}}}) => {
+const VideoPlayerScreen = ({
+  route: {
+    params: {videoData},
+  },
+}) => {
+  return (
+    <View style={styles.container}>
+        <VideoPlayerHeader title={videoData.title} />
 
-
-    return (
-        <View style={styles.container}>
-            <Text>{videoData.title}</Text>
-
-            <View style={styles.videoContainer}>
-            <WebView source={{uri: `https://www.youtube.com/embed/${videoData.videoUrl}`}} />
-
-            </View>
-
-        </View>
-    );
+      <View style={styles.videoContainer}>
+        <WebView
+          source={{uri: `https://www.youtube.com/embed/${videoData.videoUrl}`}}
+        />
+      </View>
+    </View>
+  );
 };
 
 // define your styles
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#2c3e50',
-    },
-    videoContainer:{
-        width:500,
-        height:300,
-        backgroundColor:'red'
-    }
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: primaryGradient,
+  },
+  videoContainer: {
+    width: '95%',
+    height: 300,
+    backgroundColor: 'red',
+  },
 });
 
 //make this component available to the app
